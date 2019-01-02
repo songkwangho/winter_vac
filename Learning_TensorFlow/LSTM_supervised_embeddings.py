56bf45020c2eb4522f39c46bb02c1a44f2834abc
songkwangho <46095118+songkwangho@users.noreply.github.com>
2019-01-03 00:28:57 +0900

    learning tensorflow chapter 5 complete

---
+++ Learning_TensorFlow/LSTM_supervised_embeddings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 15:33:16 2019
+
+@author: admin
+"""
+
+import numpy as np
+import tensorflow as tf
+batch_size=128
+embedding_dimension=64
+num_classes=2
+hidden_layer_size=32
+times_steps=6
+element_size=1
